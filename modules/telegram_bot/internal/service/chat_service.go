f8c69884559545ef7bdbe423db80fd2513e64970
Oleksandr Karakai <oleksandr.karakai@cmadmins-MacBook-Pro.local>
2023-04-11 16:40:02 +0300

    added greeting msg

--- modules/telegram_bot/internal/service/chat_service.go
+++ modules/telegram_bot/internal/service/chat_service.go
@@ -4,9 +4,23 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/sashabaranov/go-openai"
 	"go.uber.org/fx"
+	"log"
 	"telegram_bot/pkg/chat_gpt"
 	"telegram_bot/pkg/telegram_bot"
 )
+
+func SendGreetingMessage(update tgbotapi.Update, bot *telegram_bot.TelegramBotImpl) bool {
+	if update.Message.IsCommand() && update.Message.Command() == "start" {
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Привіт, цей бот звязанний з ChatGPT, тому можеш сміливо писати все, що захочеш")
+		_, err := bot.Bot.Send(msg)
+		if err != nil {
+			log.Println(err)
+		}
+		return true
+	}
+
+	return false
+}
 
 func SendMessage(lc fx.Lifecycle, bot *telegram_bot.TelegramBotImpl, chatGPT *chat_gpt.ChatGptImpl) {
 	messages := make([]openai.ChatCompletionMessage, 0)
@@ -16,6 +30,12 @@ func SendMessage(lc fx.Lifecycle, bot *telegram_bot.TelegramBotImpl, chatGPT *ch
 			Role:    openai.ChatMessageRoleUser,
 			Content: update.Message.Text,
 		})
+
+		isGreetingMessage := SendGreetingMessage(update, bot)
+
+		if isGreetingMessage {
+			return
+		}
 
 		update.Message.Text, _ = chatGPT.SendChatGPT(messages)
 
